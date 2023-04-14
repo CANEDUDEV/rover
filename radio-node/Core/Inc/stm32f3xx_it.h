b145a9035508f7bc3cdc2be1acab7d27525a9ba4
Hashem Hashem <hashem@canedudev.com>
2023-04-14 14:55:50 +0200

    feat(radio): working SBUS packet reception

--- radio-node/Core/Inc/stm32f3xx_it.h
+++ radio-node/Core/Inc/stm32f3xx_it.h
@@ -55,6 +55,7 @@ void DebugMon_Handler(void);
 void SysTick_Handler(void);
 void DMA1_Channel1_IRQHandler(void);
 void USB_HP_CAN_TX_IRQHandler(void);
+void USART1_IRQHandler(void);
 void DMA2_Channel1_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
