3fdf3c42a7691f285efab53829488b61e8d2df8e
Hashem Hashem <hashem@canedudev.com>
2023-04-26 11:40:53 +0200

    feat: reserve flash sector for writing conf files
    Reserve the last sector of the flash memory (2KB) for user application
    files. We will use this as a persistent storage for configuration
    parameters. Also add utility functions for writing, reading and erasing
    this sector.

--- common/utils/include/utils.h
+++ common/utils/include/utils.h
@@ -10,6 +10,14 @@
 
 #define CAN_MAX_DLC 8
 
+// 1 page (2KiB) is reserved for persistent storage
+#define FLASH_RW_START 0x0807F800
+#define FLASH_RW_END (FLASH_RW_START + 2 * 1024)
+
+// Simple error reporting codes for user applications
+#define APP_OK 0
+#define APP_NOT_OK 1
+
 typedef struct {
   uint32_t id;
   uint8_t dlc;
@@ -20,5 +28,17 @@ typedef struct {
 void Print(char *str);
 // Send task notification to task
 void NotifyTask(osThreadId_t taskHandle);
+
+// Setup the writeable flash memory area
+int FlashRWInit(void);
+
+// Erase the persistent storage. This needs to be done prior to first time
+// programming of the flash. Returns 0 on success, 1 otherwise.
+int EraseFlash(void);
+// Read from FLASH_RW area. Return 0 on success, 1 otherwise.
+int ReadFlash(uint32_t addr, void *data, size_t len);
+// Write to FLASH_RW area. The len parameter needs to be word-aligned (32-bit)
+// since we program in word mode. Return 0 on success, 1 otherwise.
+int WriteFlash(uint32_t addr, const void *data, size_t len);
 
 #endif
--- common/utils/src/utils.c
+++ common/utils/src/utils.c
@@ -3,7 +3,11 @@
 #include <string.h>
 
 #include "main.h"
+#include "stm32f3xx_hal.h"
 #include "task.h"
+
+#define FLASH_ERASE_OK 0xFFFFFFFF  // Specified by STM32 HAL
+#define FLASH_ERASED_SYMBOL 0xDEADBEEF
 
 extern UART_HandleTypeDef huart1;
 
@@ -15,4 +19,82 @@ void NotifyTask(osThreadId_t taskHandle) {
   BaseType_t xHigherPriorityTaskWoken = pdFALSE;
   vTaskNotifyGiveFromISR(taskHandle, &xHigherPriorityTaskWoken);
   portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+}
+
+/* Check if we have erased the flash by checking for existence of
+ * FLASH_ERASED_SYMBOL. If we haven't, we erase it then write the
+ * FLASH_ERASED_SYMBOL to end of writable area. Erasing flash is only needed
+ * before first time use, which is why we write the FLASH_ERASED_SYMBOL.
+ */
+int FlashRWInit(void) {
+  uint32_t flashErased = 0;
+  if (ReadFlash(FLASH_RW_END - sizeof(flashErased), &flashErased,
+                sizeof(flashErased)) != APP_OK) {
+    return APP_NOT_OK;
+  }
+  // Check if flash is already erased
+  if (flashErased == FLASH_ERASED_SYMBOL) {
+    return APP_OK;
+  }
+  EraseFlash();
+  flashErased = FLASH_ERASED_SYMBOL;
+  if (WriteFlash(FLASH_RW_END - sizeof(uint32_t), &flashErased,
+                 sizeof(flashErased)) != APP_OK) {
+    return APP_NOT_OK;
+  }
+  return APP_OK;
+}
+
+int EraseFlash(void) {
+  // 1 page = 2KB
+  FLASH_EraseInitTypeDef eraseConf = {
+      .TypeErase = FLASH_TYPEERASE_PAGES,
+      .PageAddress = FLASH_RW_START,
+      .NbPages = 1,
+  };
+  HAL_FLASH_Unlock();
+  uint32_t err = 0;
+  HAL_FLASHEx_Erase(&eraseConf, &err);
+  HAL_FLASH_Lock();
+  if (err != FLASH_ERASE_OK) {
+    return APP_NOT_OK;
+  }
+  return APP_OK;
+}
+
+int ReadFlash(uint32_t addr, void *data, size_t len) {
+  // Check if within writeable memory bounds
+  if (addr < FLASH_RW_START) {
+    return APP_NOT_OK;
+  }
+  if (addr + len > FLASH_RW_END) {
+    return APP_NOT_OK;
+  }
+  memcpy(data, (const void *)addr, len);
+  return APP_OK;
+}
+
+int WriteFlash(uint32_t addr, const void *data, size_t len) {
+  // Check if within writeable memory bounds
+  if (addr < FLASH_RW_START) {
+    return APP_NOT_OK;
+  }
+  if (addr + len > FLASH_RW_END) {
+    return APP_NOT_OK;
+  }
+  // Check if data is word-aligned
+  if (len % 4 > 0) {
+    return APP_NOT_OK;
+  }
+
+  const size_t wordLength = 4;
+  uint32_t word = 0;
+  HAL_FLASH_Unlock();
+  for (size_t i = 0; i < len; i += wordLength) {
+    memcpy(&word, data + i, wordLength);
+    HAL_FLASH_Program(FLASH_TYPEPROGRAM_WORD, addr + i, (uint64_t)word);
+  }
+  HAL_FLASH_Lock();
+
+  return APP_OK;
 }
