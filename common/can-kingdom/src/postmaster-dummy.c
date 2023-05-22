f0cee5ee3db8d5b43d07d1b5e243ecc67df21fd5
Hashem Hashem <hashem@canedudev.com>
2023-05-22 09:54:15 +0200

    fix(can-kingdom): specify dlc in ck_send_letter()

--- common/can-kingdom/src/postmaster-dummy.c
+++ common/can-kingdom/src/postmaster-dummy.c
@@ -1,6 +1,9 @@
 #include "postmaster.h"
 
-ck_err_t ck_send_letter(const ck_letter_t *letter) {
+ck_err_t ck_send_letter(const ck_letter_t *letter, uint8_t dlc) {
   (void)letter;
+  if (dlc > CK_CAN_MAX_DLC) {
+    return CK_ERR_INVALID_CAN_DLC;
+  }
   return CK_OK;
 }
