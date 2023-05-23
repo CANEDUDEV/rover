658c35789d3ebb5825a3431feba7a11624f243d3
Hashem Hashem <hashem@canedudev.com>
2023-05-23 19:41:43 +0200

    chore(apps): move Error_Handler and assert_failed functions
    These look the same for all apps so move them to common.

--- apps/radio-node/include/main.h
+++ apps/radio-node/include/main.h
@@ -40,7 +40,6 @@ extern "C" {
 /* Exported macro ------------------------------------------------------------*/
 
 /* Exported functions prototypes ---------------------------------------------*/
-void Error_Handler(void);
 
 /* Private defines -----------------------------------------------------------*/
 #define CAN_FD_INT_Pin GPIO_PIN_7
