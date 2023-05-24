8b9f39055b8d04129d148c7f9cd31873109e164a
Hashem Hashem <hashem@canedudev.com>
2023-05-24 11:44:44 +0200

    chore: refactor directories

--- libs/stm32-common/include/FreeRTOSConfig.h
+++ libs/stm32-common/include/FreeRTOSConfig.h
@@ -57,11 +57,11 @@ void xPortSysTickHandler(void);
 #define configUSE_IDLE_HOOK 0
 #define configUSE_TICK_HOOK 0
 #define configCPU_CLOCK_HZ (SystemCoreClock)
-#define configTICK_RATE_HZ ((TickType_t)1000)
-#define configMAX_PRIORITIES (56)
-#define configMINIMAL_STACK_SIZE ((uint16_t)128)
-#define configTOTAL_HEAP_SIZE ((size_t)3072)
-#define configMAX_TASK_NAME_LEN (16)
+#define configTICK_RATE_HZ 1000
+#define configMAX_PRIORITIES 56
+#define configMINIMAL_STACK_SIZE 128
+#define configTOTAL_HEAP_SIZE 10240
+#define configMAX_TASK_NAME_LEN 16
 #define configUSE_TRACE_FACILITY 1
 #define configUSE_16_BIT_TICKS 0
 #define configUSE_MUTEXES 1
