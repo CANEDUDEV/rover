9434901ffb653bce11f6834dd5c1b71d6ca06842
Hashem Hashem <hashem@canedudev.com>
2023-05-29 15:38:49 +0200

    chore(flash): rename to snake case

--- libs/stm32-common/src/flash.c
+++ libs/stm32-common/src/flash.c
@@ -14,19 +14,19 @@
  * before first time use, which is why we write the FLASH_ERASED_SYMBOL.
  */
 int flash_init(void) {
-  uint32_t flashErased = 0;
-  if (flash_read(FLASH_RW_END - sizeof(flashErased), &flashErased,
-                 sizeof(flashErased)) != APP_OK) {
+  uint32_t flash_erased = 0;
+  if (flash_read(FLASH_RW_END - sizeof(flash_erased), &flash_erased,
+                 sizeof(flash_erased)) != APP_OK) {
     return APP_NOT_OK;
   }
   // Check if flash is already erased
-  if (flashErased == FLASH_ERASED_SYMBOL) {
+  if (flash_erased == FLASH_ERASED_SYMBOL) {
     return APP_OK;
   }
   flash_erase();
-  flashErased = FLASH_ERASED_SYMBOL;
-  if (flash_write(FLASH_RW_END - sizeof(uint32_t), &flashErased,
-                  sizeof(flashErased)) != APP_OK) {
+  flash_erased = FLASH_ERASED_SYMBOL;
+  if (flash_write(FLASH_RW_END - sizeof(uint32_t), &flash_erased,
+                  sizeof(flash_erased)) != APP_OK) {
     return APP_NOT_OK;
   }
   return APP_OK;
@@ -34,14 +34,14 @@ int flash_init(void) {
 
 int flash_erase(void) {
   // 1 page = 2KB
-  FLASH_EraseInitTypeDef eraseConf = {
+  FLASH_EraseInitTypeDef erase_conf = {
       .TypeErase = FLASH_TYPEERASE_PAGES,
       .PageAddress = FLASH_RW_START,
       .NbPages = 1,
   };
   HAL_FLASH_Unlock();
   uint32_t err = 0;
-  HAL_FLASHEx_Erase(&eraseConf, &err);
+  HAL_FLASHEx_Erase(&erase_conf, &err);
   HAL_FLASH_Lock();
   if (err != FLASH_ERASE_OK) {
     return APP_NOT_OK;
@@ -74,11 +74,11 @@ int flash_write(uint32_t addr, const void *data, size_t len) {
     return APP_NOT_OK;
   }
 
-  const size_t wordLength = 4;
+  const size_t word_length = 4;
   uint32_t word = 0;
   HAL_FLASH_Unlock();
-  for (size_t i = 0; i < len; i += wordLength) {
-    memcpy(&word, (uint8_t *)data + i, wordLength);
+  for (size_t i = 0; i < len; i += word_length) {
+    memcpy(&word, (uint8_t *)data + i, word_length);
     HAL_FLASH_Program(FLASH_TYPEPROGRAM_WORD, addr + i, (uint64_t)word);
   }
   HAL_FLASH_Lock();
