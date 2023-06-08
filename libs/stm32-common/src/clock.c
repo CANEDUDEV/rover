1b582a5f9361fa85aa9471a0bf243539e2214126
Hashem Hashem <hashem@canedudev.com>
2023-06-08 17:25:44 +0200

    fix(clock): init clocks for all boards' peripherals
    Some clocks were missing.

--- libs/stm32-common/src/clock.c
+++ libs/stm32-common/src/clock.c
@@ -2,9 +2,9 @@
 #include "stm32f3xx_hal.h"
 
 void system_clock_init(void) {
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+  RCC_PeriphCLKInitTypeDef PeriphClkInit;
 
   /** Initializes the RCC Oscillators according to the specified parameters
    * in the RCC_OscInitTypeDef structure.
@@ -33,10 +33,14 @@ void system_clock_init(void) {
     error();
   }
   PeriphClkInit.PeriphClockSelection =
-      RCC_PERIPHCLK_USART1 | RCC_PERIPHCLK_I2C1 | RCC_PERIPHCLK_ADC12;
-  PeriphClkInit.Usart1ClockSelection = RCC_USART1CLKSOURCE_PCLK2;
+      RCC_PERIPHCLK_ADC12 | RCC_PERIPHCLK_I2C1 | RCC_PERIPHCLK_I2C3 |
+      RCC_PERIPHCLK_TIM1 | RCC_PERIPHCLK_USART1 | RCC_PERIPHCLK_USART2;
   PeriphClkInit.Adc12ClockSelection = RCC_ADC12PLLCLK_DIV1;
   PeriphClkInit.I2c1ClockSelection = RCC_I2C1CLKSOURCE_SYSCLK;
+  PeriphClkInit.I2c3ClockSelection = RCC_I2C3CLKSOURCE_SYSCLK;
+  PeriphClkInit.Tim1ClockSelection = RCC_TIM1CLK_HCLK;
+  PeriphClkInit.Usart1ClockSelection = RCC_USART1CLKSOURCE_SYSCLK;
+  PeriphClkInit.Usart2ClockSelection = RCC_USART2CLKSOURCE_SYSCLK;
   if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK) {
     error();
   }
