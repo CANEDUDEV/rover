063e610e88015479c323ac0e244965b3edc0de8f
Hashem Hashem <hashem@canedudev.com>
2023-06-08 11:01:39 +0200

    fix(apps): init DMA before ADC peripherals
    DMA must be configured before ADC since ADC is configured in DMA mode.

--- apps/battery-node/src/peripherals.c
+++ apps/battery-node/src/peripherals.c
@@ -25,11 +25,11 @@ void peripherals_init(void) {
   common_peripherals_init();
   peripherals.common_peripherals = get_common_peripherals();
 
+  gpio_init();
+  dma_init();  // Must be called before ADC init functions.
   adc1_init();
   adc2_init();
   i2c1_init();
-  dma_init();
-  gpio_init();
 }
 
 /**
