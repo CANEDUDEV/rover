bfc19cefaf4b0c8636ff0e49783e0a775f70b231
Hashem Hashem <hashem@canedudev.com>
2023-06-30 15:31:54 +0200

    feat(integration): add servo support in scripts

--- integration/ck-startup.py
+++ integration/ck-startup.py
@@ -1,7 +1,15 @@
 import ck
 import battery
+import servo
+import sys
 
 from canlib import canlib
+
+if len(sys.argv) <= 1:
+    print("specify card")
+    exit(1)
+
+app = str(sys.argv[1])
 
 with canlib.openChannel(
     channel=0,
@@ -13,8 +21,23 @@ with canlib.openChannel(
 
     ch.writeWait(ck.default_letter, -1)
     ch.writeWait(ck.give_base_no, -1)
-    ch.writeWait(battery.assign_folder2, -1)
-    ch.writeWait(battery.assign_folder3, -1)
-    ch.writeWait(battery.assign_folder4, -1)
+
+    if app == "battery":
+        ch.writeWait(battery.assign_folder2, -1)
+        ch.writeWait(battery.assign_folder3, -1)
+        ch.writeWait(battery.assign_folder4, -1)
+        ch.writeWait(battery.assign_folder5, -1)
+        ch.writeWait(battery.assign_folder6, -1)
+        ch.writeWait(battery.assign_folder7, -1)
+        ch.writeWait(battery.assign_folder8, -1)
+        ch.writeWait(battery.assign_folder9, -1)
+
+    elif app == "servo":
+        ch.writeWait(servo.assign_folder2, -1)
+        ch.writeWait(servo.assign_folder3, -1)
+        ch.writeWait(servo.assign_folder4, -1)
+        ch.writeWait(servo.assign_folder5, -1)
+        ch.writeWait(servo.assign_folder6, -1)
+
     ch.writeWait(ck.communicate, -1)
     ch.busOff()
