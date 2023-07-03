429f531159330e49f13279eb668615de365ad828
Hashem Hashem <hashem@canedudev.com>
2023-07-03 21:55:39 +0200

    feat(sbus-receiver): add sbus packet parsing

--- apps/sbus-receiver/src/main.c
+++ apps/sbus-receiver/src/main.c
@@ -1,5 +1,3 @@
-#include <stm32f3xx_hal.h>
-#include <stm32f3xx_hal_can.h>
 #include <string.h>
 
 #include "clock.h"
@@ -8,21 +6,17 @@
 #include "flash.h"
 #include "peripherals.h"
 #include "print.h"
+#include "stm32f3xx_hal.h"
 
 // FreeRTOS
 #include "FreeRTOS.h"
 #include "queue.h"
+#include "sbus.h"
 #include "task.h"
 
-// Hardware
-static peripherals_t *peripherals;
-
 // Application
-#define CAN_BASE_ID 100U
+#define CAN_BASE_ID 0x100
 #define CAN_MAX_DLC 8
-
-#define SBUS_PACKET_LENGTH 25
-#define SBUS_HEADER 0x0F
 
 typedef struct {
   uint32_t id;
@@ -31,14 +25,13 @@ typedef struct {
 } CANFrame;
 
 // FreeRTOS
-#define IO_READ_TASK_PERIOD_MS 20
 #define TASK_PRIORITY 24
 
 static TaskHandle_t sbus_read_task;
 static StaticTask_t sbus_read_buffer;
 static StackType_t sbus_read_stack[configMINIMAL_STACK_SIZE];
 
-void sbus_read(void *argument);
+void sbus_read(void *unused);
 
 /**
  * @brief  The application entry point.
@@ -53,7 +46,6 @@ int main(void) {
 
   // Initialize all configured peripherals
   peripherals_init();
-  peripherals = get_peripherals();
 
   sbus_read_task =
       xTaskCreateStatic(sbus_read, "sbus read", configMINIMAL_STACK_SIZE, NULL,
@@ -69,46 +61,51 @@ int main(void) {
   }
 }
 
-void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
-  (void)huart;
-  BaseType_t higher_priority_task_woken = pdFALSE;
-  vTaskNotifyGiveFromISR(sbus_read_task, &higher_priority_task_woken);
-  portYIELD_FROM_ISR(higher_priority_task_woken);
-}
-
-void sbus_read(void *argument) {
-  UNUSED(argument);
-
-  uint8_t sbusHeader = 0;
-  uint8_t sbusData[SBUS_PACKET_LENGTH];
+void sbus_read(void *unused) {
+  (void)unused;
 
   peripherals_t *peripherals = get_peripherals();
 
-  // Wait until reception of one complete message, in case we power up in the
-  // middle of a transmission
-  while (sbusHeader != SBUS_HEADER) {
-    HAL_UART_Receive(&peripherals->huart2, &sbusHeader, sizeof(sbusHeader),
-                     HAL_MAX_DELAY);
-  }
-  HAL_UART_Receive(&peripherals->huart2, sbusData, sizeof(sbusData) - 1,
-                   HAL_MAX_DELAY);
-
-  CANFrame frames[3];
-  for (int i = 0; i < 3; i++) {
+  CANFrame frames[4];
+  for (int i = 0; i < 4; i++) {
     frames[i].id = CAN_BASE_ID + i;
     frames[i].dlc = CAN_MAX_DLC;
   }
 
   uint32_t mailbox = 0;
 
-  HAL_CAN_Start(&peripherals->common_peripherals->hcan);
-  for (;;) {
-    memset(sbusData, 0, sizeof(sbusData));
-    HAL_UART_Receive_IT(&peripherals->huart2, sbusData, sizeof(sbusData));
-    ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+  uint8_t sbus_data[SBUS_PACKET_LENGTH];
+  sbus_packet_t sbus_packet;
 
-    for (int i = 0; i < 3; i++) {
-      memcpy(frames[i].data, &sbusData[i * CAN_MAX_DLC], CAN_MAX_DLC);
+  HAL_CAN_Start(&peripherals->common_peripherals->hcan);
+
+  for (;;) {
+    memset(sbus_data, 0, sizeof(sbus_data));
+
+    // Wait until reception of one complete message, in case we power up in the
+    // middle of a transmission
+    while (sbus_data[0] != SBUS_HEADER) {
+      HAL_UART_Receive(&peripherals->huart2, sbus_data, 1, 3);
+    }
+
+    HAL_UART_Receive_IT(&peripherals->huart2, &sbus_data[1],
+                        sizeof(sbus_data) - 1);
+    ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+    sbus_parse_data(sbus_data, &sbus_packet);
+
+    // Failsafe usually triggers if many frames are lost in a row
+    // Indicates connection loss (heavy)
+    if (sbus_packet.failsafe_activated) {
+      print("Failsafe activated\r\n");
+    }
+
+    // Indicates slight connection loss or issue with frame.
+    if (sbus_packet.frame_lost) {
+      print("Frame lost\r\n");
+    }
+
+    for (int i = 0; i < 4; i++) {
+      memcpy(frames[i].data, &sbus_packet.channels[i * 4], CAN_MAX_DLC);
       while (HAL_CAN_GetTxMailboxesFreeLevel(
                  &peripherals->common_peripherals->hcan) < 1) {
       }
@@ -122,4 +119,11 @@ void sbus_read(void *argument) {
                            frames[i].data, &mailbox);
     }
   }
+}
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
+  (void)huart;
+  BaseType_t higher_priority_task_woken = pdFALSE;
+  vTaskNotifyGiveFromISR(sbus_read_task, &higher_priority_task_woken);
+  portYIELD_FROM_ISR(higher_priority_task_woken);
 }
