6892facbe71b258c5be73d9093f480c5fa268449
Hashem Hashem <hashem@canedudev.com>
2023-07-04 11:59:08 +0200

    feat: assign city addresses
    Also compile two versions of the servo binary, one for a steering servo
    and one for a motor. The only difference between the binaries is the
    city address.

--- integration/ck-startup.py
+++ integration/ck-startup.py
@@ -2,15 +2,8 @@ import ck
 import battery
 import sbus
 import servo
-import sys
 
 from canlib import canlib
-
-if len(sys.argv) <= 1:
-    print("specify card")
-    exit(1)
-
-app = str(sys.argv[1])
 
 with canlib.openChannel(
     channel=0,
@@ -23,32 +16,29 @@ with canlib.openChannel(
     ch.writeWait(ck.default_letter, -1)
     ch.writeWait(ck.give_base_no, -1)
 
-    if app == "battery":
-        ch.writeWait(battery.assign_folder2, -1)
-        ch.writeWait(battery.assign_folder3, -1)
-        ch.writeWait(battery.assign_folder4, -1)
-        ch.writeWait(battery.assign_folder5, -1)
-        ch.writeWait(battery.assign_folder6, -1)
-        ch.writeWait(battery.assign_folder7, -1)
-        ch.writeWait(battery.assign_folder8, -1)
-        ch.writeWait(battery.assign_folder9, -1)
+    ch.writeWait(battery.assign_folder2, -1)
+    ch.writeWait(battery.assign_folder3, -1)
+    ch.writeWait(battery.assign_folder4, -1)
+    ch.writeWait(battery.assign_folder5, -1)
+    ch.writeWait(battery.assign_folder6, -1)
+    ch.writeWait(battery.assign_folder7, -1)
+    ch.writeWait(battery.assign_folder8, -1)
+    ch.writeWait(battery.assign_folder9, -1)
 
-    elif app == "servo":
-        ch.writeWait(servo.assign_folder2, -1)
-        ch.writeWait(servo.assign_folder3, -1)
-        ch.writeWait(servo.assign_folder4, -1)
-        ch.writeWait(servo.assign_folder5, -1)
-        ch.writeWait(servo.assign_folder6, -1)
-        ch.writeWait(servo.assign_folder7, -1)
-        ch.writeWait(servo.assign_folder8, -1)
-        ch.writeWait(servo.assign_folder9, -1)
-        ch.writeWait(servo.assign_folder10, -1)
+    ch.writeWait(servo.assign_folder2, -1)
+    ch.writeWait(servo.assign_folder3, -1)
+    ch.writeWait(servo.assign_folder4, -1)
+    ch.writeWait(servo.assign_folder5, -1)
+    ch.writeWait(servo.assign_folder6, -1)
+    ch.writeWait(servo.assign_folder7, -1)
+    ch.writeWait(servo.assign_folder8, -1)
+    ch.writeWait(servo.assign_folder9, -1)
+    ch.writeWait(servo.assign_folder10, -1)
 
-    elif app == "sbus":
-        ch.writeWait(sbus.assign_folder2, -1)
-        ch.writeWait(sbus.assign_folder3, -1)
-        ch.writeWait(sbus.assign_folder4, -1)
-        ch.writeWait(sbus.assign_folder5, -1)
+    ch.writeWait(sbus.assign_folder2, -1)
+    ch.writeWait(sbus.assign_folder3, -1)
+    ch.writeWait(sbus.assign_folder4, -1)
+    ch.writeWait(sbus.assign_folder5, -1)
 
     ch.writeWait(ck.communicate, -1)
     ch.busOff()
--- integration/sbus.py
+++ integration/sbus.py
@@ -1,19 +1,23 @@
 # SBUS receiver testing frames
 
 from canlib import Frame
+import ck
 
 # Assign envelope 0x300 to folder 2
 # Steering pulse width
-assign_folder2 = Frame(id_=0, dlc=8, data=[0, 2, 0, 3, 0, 0, 2, 0x3])
+assign_folder2 = Frame(id_=0, dlc=8, data=[ck.sbus_receiver_id, 2, 0, 3, 0, 0, 2, 0x3])
 
 # Assign envelope 0x301 to folder 3
 # Steering trim pulse width (signed integer)
-assign_folder3 = Frame(id_=0, dlc=8, data=[0, 2, 1, 3, 0, 0, 3, 0x3])
+assign_folder3 = Frame(id_=0, dlc=8, data=[ck.sbus_receiver_id, 2, 1, 3, 0, 0, 3, 0x3])
 
 # Assign envelope 0x302 to folder 4
 # Throttle pulse width
-assign_folder4 = Frame(id_=0, dlc=8, data=[0, 2, 2, 3, 0, 0, 4, 0x3])
+assign_folder4 = Frame(id_=0, dlc=8, data=[ck.sbus_receiver_id, 2, 2, 3, 0, 0, 4, 0x3])
 
 # Assign envelope 0x303 to folder 5
 # Throttle trim pulse width (signed integer)
-assign_folder5 = Frame(id_=0, dlc=8, data=[0, 2, 3, 3, 0, 0, 5, 0x3])
+assign_folder5 = Frame(id_=0, dlc=8, data=[ck.sbus_receiver_id, 2, 3, 3, 0, 0, 5, 0x3])
+
+# Assign steering pulse width to servo's address
+assign_steering = Frame(id_=0, dlc=3, data=[ck.sbus_receiver_id, 2, 0, 3, 0, 0, 2, 0x3])
