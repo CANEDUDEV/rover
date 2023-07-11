51c03b31b2dcdf6cc71c3be32bf3b5b943ed14cf
Hashem Hashem <hashem@canedudev.com>
2023-07-11 17:08:00 +0200

    fix(scripts): run black on whole repo
    This checks sphinx files.

--- docs/conf.py
+++ docs/conf.py
@@ -6,27 +6,27 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Rover'
-copyright = '2023, CANEDUDEV AB'
-author = 'CANEDUDEV AB'
+project = "Rover"
+copyright = "2023, CANEDUDEV AB"
+author = "CANEDUDEV AB"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [ 'breathe', 'sphinx_rtd_theme' ]
+extensions = ["breathe", "sphinx_rtd_theme"]
 
 breathe_default_project = "Rover"
 
 # Tell sphinx what the primary language being documented is.
-primary_domain = 'c'
+primary_domain = "c"
 
 # Tell sphinx what the pygments highlight language should be.
-highlight_language = 'c'
+highlight_language = "c"
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
