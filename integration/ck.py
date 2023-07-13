6c03f1f267106bc5d31f2b5c04523e44ccd39d67
Hashem Hashem <hashem@canedudev.com>
2023-07-13 14:19:29 +0200

    feat(integration): add rover startup script and associated DBC
    Also remove now obsolete DBC files.

--- integration/ck.py
+++ integration/ck.py
@@ -4,8 +4,8 @@ from canlib import Frame
 
 default_letter = Frame(id_=2031, data=[0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA])
 
-# Give base number and ask for response page 0
-give_base_no = Frame(id_=0, dlc=8, data=[0, 1, 0, 0, 0, 2, 0, 0])
+# Give base number (0x400) and ask for response page 0
+give_base_no = Frame(id_=0, dlc=8, data=[0, 1, 0, 0, 0, 4, 0, 0])
 
 # Set communication mode to COMMUNICATE
 communicate = Frame(id_=0, dlc=8, data=[0, 0, 0, 0x3, 0, 0, 0, 0])
