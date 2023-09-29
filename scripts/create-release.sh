0ef40d449d3df7ec9a887c399ef490ab87b3a129
Hashem Hashem <hashem@canedudev.com>
2023-09-29 10:14:17 +0200

    chore(release): add DBC file to release and rename zip
    Rename the release zip file to include the latest git tag.
    Also add the rover.dbc file to release.

--- scripts/create-release.sh
+++ scripts/create-release.sh
@@ -2,6 +2,13 @@
 
 set -eo pipefail
 
-find . -type f -name "*.bin" | zip -q -j rover-binaries.zip -@
+TAG=$(git describe --tags)
+RELEASE_FILE="rover-release-${TAG}.zip"
+
+# Add all .bin files
+find . -type f -name "*.bin" | zip -q -j "${RELEASE_FILE}" -@
+
+# Add DBC file
+zip -q -u "${RELEASE_FILE}" rover.dbc
 
 echo "Finished creating release."
