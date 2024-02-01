192e8764e4281c999b55a793c0d64037a4cb7565
Hashem Hashem <hashem@canedudev.com>
2024-02-01 15:53:15 +0100

    feat(battery-monitor): add function to convert ADC value to VBAT output voltage
    A new function `adc_to_vbat_out_voltage` has been added to the ADC
    module to convert ADC values to the corresponding VBAT output voltage
    This function utilizes a voltage divider calculation similar to the
    existing `adc_to_cell_voltage` function.
    
    The implementation includes static function `adc_value_to_voltage` for
    converting raw ADC values to voltages, and a `divide_voltage` function
    that applies the voltage divider formula.
    
    The `voltage_divider_t` and `lt6106_current_sensor_t`
    structs have been introduced to encapsulate the parameters for these
    calculations, improving code readability and maintainability.

--- apps/battery-monitor/src/adc.c
+++ apps/battery-monitor/src/adc.c
@@ -2,85 +2,97 @@
 
 #include <math.h>
 
-#include "battery.h"
-
 #define ADC_REF_VOLTAGE 3300            // in mV
 #define ADC_RESOLUTION ((1 << 12) - 1)  // 12-bit ADC
 
 static float vbat_sense_r_out;
 
-/* Voltage divider with R1 = 13kOhm and R2 = 1.6kOhm
- * v_cell = v_out * (R1 + R2) / R2
- * v_out is measured by ADC, v_out = ADC_REF_VOLTAGE * adc_value /
- * ADC_RESOLUTION
- */
-uint16_t adc_to_cell_voltage(const uint16_t adc_value) {
-  float v_out = ADC_REF_VOLTAGE * adc_value / (float)ADC_RESOLUTION;
-  const float numerator = v_out * (13000 + 1600);
-  const float denominator = 1600;
-  float cell_voltage = numerator / denominator;
-  return (uint16_t)roundf(cell_voltage);
+typedef struct {
+  uint16_t r1;
+  uint16_t r2;
+} voltage_divider_t;
+
+typedef struct {
+  float r_in;
+  float r_out;
+  float r_sense;
+} lt6106_current_sensor_t;
+
+// Returns voltage in mV.
+static float adc_value_to_voltage(uint16_t adc_value) {
+  return ADC_REF_VOLTAGE * adc_value / (float)ADC_RESOLUTION;
+}
+
+static float divide_voltage(float voltage, const voltage_divider_t *divider) {
+  return voltage * (float)(divider->r1 + divider->r2) / (float)divider->r2;
 }
 
 /* The LT6106 current sensor specifies that
- * i_sense = v_out * r_in / (r_sense * r_out).
- * v_out is measured by ADC, v_out = ADC_REF_VOLTAGE * adc_value /
- * ADC_RESOLUTION
+ * i_sense = voltage * r_in / (r_sense * r_out).
  *
- * Resistances for Battery Node rev B:
- * r_in = 51 Ohm
- * r_sense = 0.005 Ohm
- * r_out = 5100 Ohm
+ * Returns current in mA.
  */
-uint16_t adc_to_reg_out_current(const uint16_t adc_value) {
-  float v_out = ADC_REF_VOLTAGE * adc_value / (float)ADC_RESOLUTION;
-  const float r_in = 51;
-  const float r_out = 5100;
-  // Invert r_sense to avoid floating point, 1/0.005Ohm = 200 Ohm
-  const float inv_r_sense = 200;
+static float sense_current(float voltage,
+                           const lt6106_current_sensor_t *sensor) {
+  return voltage * sensor->r_in / (sensor->r_sense * sensor->r_out);
+}
 
-  // Multiply by inv_r_sense instead of dividing by r_sense
-  float i_sense = v_out * r_in * inv_r_sense / r_out;  // in mA
+uint16_t adc_to_cell_voltage(uint16_t adc_value) {
+  const voltage_divider_t divider = {
+      .r1 = 13000,
+      .r2 = 1600,
+  };
+  float measured_voltage = adc_value_to_voltage(adc_value);
+  float cell_voltage = divide_voltage(measured_voltage, &divider);
+  return (uint16_t)roundf(cell_voltage);
+}
+
+uint16_t adc_to_reg_out_current(uint16_t adc_value) {
+  const lt6106_current_sensor_t sensor = {
+      .r_in = 51,
+      .r_out = 5100,
+      .r_sense = 0.005F,
+  };
+  float measured_voltage = adc_value_to_voltage(adc_value);
+  float i_sense = sense_current(measured_voltage, &sensor);
   return (uint16_t)roundf(i_sense);
 }
 
-/* Voltage divider with R1 = 47kOhm and R2 = 10kOhm
- * reg_vout = v_out * (R1 + R2) / R2
- * v_out is measured by ADC, v_out = ADC_REF_VOLTAGE * adc_value /
- * ADC_RESOLUTION
- */
 uint16_t adc_to_reg_out_voltage(uint16_t adc_value) {
-  float v_out = ADC_REF_VOLTAGE * adc_value / (float)ADC_RESOLUTION;
-  const float numerator = v_out * (47000 + 10000);
-  const float denominator = 10000;
-  float reg_vout = numerator / denominator;
+  const voltage_divider_t divider = {
+      .r1 = 47000,
+      .r2 = 10000,
+  };
+  float measured_voltage = adc_value_to_voltage(adc_value);
+  float reg_vout = divide_voltage(measured_voltage, &divider);
   return (uint16_t)roundf(reg_vout);
 }
 
 /*
- * Uses LT6106 as well, with variable r_out configured by placing jumpers on X11
- * and X12.
- * i_sense = v_out * r_in / (r_sense * r_out)
- *
- * v_out is measured by ADC, v_out = ADC_REF_VOLTAGE * adc_value /
- * ADC_RESOLUTION
- *
- * Resistances:
- * r_in = 51 Ohm
- * r_sense = 0.5 mOhm
- * r_out: See vbat_sense_r_out
+ * Uses LT6106 as well, but has variable r_out configured by placing jumpers on
+ * X11 and X12.
  *
  * Note: we return uint32_t because we can measure values from 20 mA to 120 A.
  */
-uint32_t adc_to_vbat_out_current(const uint16_t adc_value) {
-  float v_out = ADC_REF_VOLTAGE * adc_value / (float)ADC_RESOLUTION;  // in mV
-  const float r_in = 51;
-  // Invert r_sense to avoid floating point, 1/0.5mOhm = 2000 Ohm
-  const float inv_r_sense = 2000;
+uint32_t adc_to_vbat_out_current(uint16_t adc_value) {
+  const lt6106_current_sensor_t sensor = {
+      .r_in = 51,
+      .r_out = vbat_sense_r_out,
+      .r_sense = 0.0005F,
+  };
+  float measured_voltage = adc_value_to_voltage(adc_value);
+  float i_sense = sense_current(measured_voltage, &sensor);
+  return (uint32_t)roundf(i_sense);
+}
 
-  // Multiply by inv_r_sense instead of dividing by r_sense
-  float i_sense = v_out * r_in * inv_r_sense / vbat_sense_r_out;  // in mA
-  return (uint16_t)roundf(i_sense);
+uint16_t adc_to_vbat_out_voltage(uint16_t adc_value) {
+  const voltage_divider_t divider = {
+      .r1 = 13000,
+      .r2 = 1600,
+  };
+  float measured_voltage = adc_value_to_voltage(adc_value);
+  float vbat_out = divide_voltage(measured_voltage, &divider);
+  return (uint16_t)roundf(vbat_out);
 }
 
 void set_jumper_config(jumper_config_t jumper_config) {
