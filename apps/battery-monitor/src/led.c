db41776b97c5041dd20bcceee322c1a7a3aa2ec7
Hashem Hashem <hashem@canedudev.com>
2024-02-13 13:27:00 +0100

    chore(battery-monitor): improve LED fault signalling
    Use a timer to blink the LEDs red.

--- apps/battery-monitor/src/led.c
+++ apps/battery-monitor/src/led.c
@@ -1,7 +1,70 @@
 #include "led.h"
 
 #include "ports.h"
-#include "stm32f3xx_hal.h"
+
+// FreeRTOS
+#include "FreeRTOS.h"
+#include "timers.h"
+
+typedef struct {
+  GPIO_TypeDef *red_led_port;
+  uint16_t red_led_pin;
+  GPIO_TypeDef *green_led_port;
+  uint16_t green_led_pin;
+} led_cfg_t;
+
+static const led_cfg_t led6_cfg = {
+    .red_led_port = LED1_GPIO_PORT,
+    .red_led_pin = LED1_PIN,
+    .green_led_port = LED2_GPIO_PORT,
+    .green_led_pin = LED2_PIN,
+};
+
+static const led_cfg_t led7_cfg = {
+    .red_led_port = LED3_GPIO_PORT,
+    .green_led_port = LED4_GPIO_PORT,
+    .red_led_pin = LED3_PIN,
+    .green_led_pin = LED4_PIN,
+};
+
+static StaticTimer_t led_timer_buf;
+static TimerHandle_t led_timer;
+
+void led_timer_callback(TimerHandle_t timer);
+void blink_leds_red(void);
+
+void led_init(void) {
+  const uint16_t led_blink_period_ms = 100;
+  led_timer =
+      xTimerCreateStatic("LED timer", pdMS_TO_TICKS(led_blink_period_ms),
+                         pdTRUE,  // Auto reload timer
+                         NULL,    // Timer ID, unused
+                         led_timer_callback, &led_timer_buf);
+}
+
+void led_signal_fault(void) {
+  if (xTimerIsTimerActive(led_timer)) {
+    return;
+  }
+
+  blink_leds_red();
+  xTimerStart(led_timer, portMAX_DELAY);
+}
+
+void led_stop_signal_fault(void) {
+  if (!xTimerIsTimerActive(led_timer)) {
+    return;
+  }
+
+  xTimerStop(led_timer, portMAX_DELAY);
+  set_led_color(LED6, NONE);
+  set_led_color(LED7, NONE);
+}
+
+void led_timer_callback(TimerHandle_t timer) {
+  (void)timer;
+  blink_leds_red();
+}
 
 void blink_leds_red(void) {
   static uint8_t blink = 0;
@@ -17,34 +80,39 @@ void blink_leds_red(void) {
 }
 
 void set_led_color(led_t led, led_color_t color) {
-  GPIO_TypeDef *red_led_port = LED1_GPIO_PORT;
-  GPIO_TypeDef *green_led_port = LED2_GPIO_PORT;
-  uint16_t red_led = LED1_PIN;
-  uint16_t green_led = LED2_PIN;
+  const led_cfg_t *led_cfg = &led6_cfg;
 
   if (led == LED7) {
-    red_led_port = LED3_GPIO_PORT;
-    green_led_port = LED4_GPIO_PORT;
-    red_led = LED3_PIN;
-    green_led = LED4_PIN;
+    led_cfg = &led7_cfg;
   }
 
   switch (color) {
     case NONE:
-      HAL_GPIO_WritePin(red_led_port, red_led, GPIO_PIN_RESET);
-      HAL_GPIO_WritePin(green_led_port, green_led, GPIO_PIN_RESET);
+      HAL_GPIO_WritePin(led_cfg->red_led_port, led_cfg->red_led_pin,
+                        GPIO_PIN_RESET);
+      HAL_GPIO_WritePin(led_cfg->green_led_port, led_cfg->green_led_pin,
+                        GPIO_PIN_RESET);
       break;
+
     case RED:
-      HAL_GPIO_WritePin(red_led_port, red_led, GPIO_PIN_SET);
-      HAL_GPIO_WritePin(green_led_port, green_led, GPIO_PIN_RESET);
+      HAL_GPIO_WritePin(led_cfg->red_led_port, led_cfg->red_led_pin,
+                        GPIO_PIN_SET);
+      HAL_GPIO_WritePin(led_cfg->green_led_port, led_cfg->green_led_pin,
+                        GPIO_PIN_RESET);
       break;
+
     case GREEN:
-      HAL_GPIO_WritePin(red_led_port, red_led, GPIO_PIN_RESET);
-      HAL_GPIO_WritePin(green_led_port, green_led, GPIO_PIN_SET);
+      HAL_GPIO_WritePin(led_cfg->red_led_port, led_cfg->red_led_pin,
+                        GPIO_PIN_RESET);
+      HAL_GPIO_WritePin(led_cfg->green_led_port, led_cfg->green_led_pin,
+                        GPIO_PIN_SET);
       break;
+
     case ORANGE:
-      HAL_GPIO_WritePin(red_led_port, red_led, GPIO_PIN_SET);
-      HAL_GPIO_WritePin(green_led_port, green_led, GPIO_PIN_SET);
+      HAL_GPIO_WritePin(led_cfg->red_led_port, led_cfg->red_led_pin,
+                        GPIO_PIN_SET);
+      HAL_GPIO_WritePin(led_cfg->green_led_port, led_cfg->green_led_pin,
+                        GPIO_PIN_SET);
       break;
   }
 }
