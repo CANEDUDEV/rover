b6a6082c1e2d682afc8c8c83c5f51f00815ac14b
Hashem Hashem <hashem@canedudev.com>
2024-02-15 16:08:46 +0100

    chore(tests): use new ASSERT macro in tests

--- libs/can-kingdom/tests/king-test.c
+++ libs/can-kingdom/tests/king-test.c
@@ -1,32 +1,29 @@
 #include "king.h"
 
-#include <assert.h>
-#include <stdio.h>
 #include <string.h>
 
 #include "ck-test.h"
 #include "test.h"
 
-static test_err_t verify_kp_header(int page_no, ck_page_t *page);
-
-static test_err_t test_kp0(void);
-static test_err_t test_kp1(void);
-static test_err_t test_kp2(void);
-static test_err_t test_kp8(void);
-static test_err_t test_kp16(void);
-static test_err_t test_kp17(void);
+void test_kp0(void);
+void test_kp1(void);
+void test_kp2(void);
+void test_kp8(void);
+void test_kp16(void);
+void test_kp17(void);
+void verify_kp_header(int page_no, ck_page_t *page);
 
 int main(void) {
-  assert(test_kp0() == TEST_PASS);
-  assert(test_kp1() == TEST_PASS);
-  assert(test_kp2() == TEST_PASS);
-  assert(test_kp8() == TEST_PASS);
-  assert(test_kp16() == TEST_PASS);
-  assert(test_kp17() == TEST_PASS);
+  test_kp0();
+  test_kp1();
+  test_kp2();
+  test_kp8();
+  test_kp16();
+  test_kp17();
 }
 
 // NOLINTBEGIN(*-magic-numbers)
-static test_err_t test_kp0(void) {
+void test_kp0(void) {
   ck_kp0_args_t args = {
       .address = test_city_address,
       .action_mode = CK_ACTION_MODE_RUN,
@@ -37,39 +34,31 @@ static test_err_t test_kp0(void) {
 
   ck_page_t page;
 
-  if (ck_create_kings_page_0(&args, &page) != CK_OK) {
-    printf("KP0: failed to create king's page.\n");
-    return TEST_FAIL;
-  }
-  if (verify_kp_header(0, &page) != TEST_PASS) {
-    return TEST_FAIL;
-  }
+  ASSERT(ck_create_kings_page_0(&args, &page) == CK_OK, "");
 
-  if (page.lines[2] != args.action_mode) {
-    printf("KP0: wrong action mode, expected: %x, got: %x.\n", args.action_mode,
-           page.lines[2]);
-    return TEST_FAIL;
-  }
-  if ((page.lines[3] & 0x3) != args.comm_mode) {
-    printf("KP0: wrong comm mode, expected: %x, got: %x.\n", args.comm_mode,
-           page.lines[3] & 0x3);
-    return TEST_FAIL;
-  }
-  if ((page.lines[3] & ~0x3) != args.comm_flags) {
-    printf("KP0: wrong flags set, expected: %x, got: %x.\n", args.comm_flags,
-           page.lines[3] & ~0x3);
-    return TEST_FAIL;
-  }
-  if (page.lines[4] != args.city_mode) {
-    printf("KP0: wrong city mode, expected: %x, got: %x.\n", args.city_mode,
-           page.lines[4]);
-    return TEST_FAIL;
-  }
+  verify_kp_header(0, &page);
 
-  return TEST_PASS;
+  ASSERT(page.lines[2] == args.action_mode,
+         "wrong action mode, expected: %x, got: %x.", args.action_mode,
+         page.lines[2]);
+
+  ck_comm_mode_t comm_mode = page.lines[3] & 0x3;
+
+  ASSERT(comm_mode == args.comm_mode, "wrong comm mode, expected: %x, got: %x.",
+         args.comm_mode, comm_mode);
+
+  ck_comm_flags_t comm_flags = page.lines[3] & ~0x3;
+
+  ASSERT(comm_flags == args.comm_flags,
+         "wrong flags set, expected: %x, got: %x.", args.comm_flags,
+         comm_flags);
+
+  ASSERT(page.lines[4] == args.city_mode,
+         "wrong city mode, expected: %x, got: %x.", args.city_mode,
+         page.lines[4]);
 }
 
-static test_err_t test_kp1(void) {
+void test_kp1(void) {
   ck_kp1_args_t args = {
       .address = test_city_address,
       .mayor_response_no = CK_NO_RESPONSE_REQUESTED,
@@ -77,56 +66,46 @@ static test_err_t test_kp1(void) {
       .has_extended_id = true,
   };
   ck_page_t page;
-  if (ck_create_kings_page_1(&args, &page) != CK_OK) {
-    printf("KP1: failed to create king's page.\n");
-    return TEST_FAIL;
-  }
-  if (verify_kp_header(1, &page) != TEST_PASS) {
-    return TEST_FAIL;
-  }
-  if (page.lines[2] != CK_NO_RESPONSE_REQUESTED) {
-    printf("KP1: wrong mayor's response page, expected: %u, got: %u.\n",
-           CK_NO_RESPONSE_REQUESTED, page.lines[2]);
-    return TEST_FAIL;
-  }
+
+  ASSERT(ck_create_kings_page_1(&args, &page) == CK_OK, "");
+
+  verify_kp_header(1, &page);
+
+  ASSERT(page.lines[2] == CK_NO_RESPONSE_REQUESTED,
+         "wrong mayor's response page, expected: %u, got: %u.",
+         CK_NO_RESPONSE_REQUESTED, page.lines[2]);
 
   uint32_t got_base_no = 0;
   memcpy(&got_base_no, &page.lines[4], sizeof(got_base_no));
   got_base_no &= CK_CAN_ID_MASK;
-  if (got_base_no != args.base_no) {
-    printf("KP1: wrong base number, expected: %u, got: %u.\n", args.base_no,
-           got_base_no);
-    return TEST_FAIL;
-  }
+
+  ASSERT(got_base_no == args.base_no,
+         "wrong base number, expected: %u, got: %u.", args.base_no,
+         got_base_no);
 
   bool got_has_extended_id = (page.lines[7] >> 7) & 0x01;
-  if (got_has_extended_id != args.has_extended_id) {
-    printf("KP1: wrong extended ID flag, expected: %u, got: %u.\n",
-           args.has_extended_id, got_has_extended_id);
-    return TEST_FAIL;
-  }
+
+  ASSERT(got_has_extended_id == args.has_extended_id,
+         "wrong extended ID flag, expected: %u, got: %u.", args.has_extended_id,
+         got_has_extended_id);
 
   // Test illegal base numbers.
   args.base_no = illegal_can_std_id;
   args.has_extended_id = false;
   memset(&page, 0, sizeof(page));
-  if (ck_create_kings_page_1(&args, &page) != CK_ERR_INVALID_CAN_ID) {
-    printf("KP1: setting illegal standard base number succeeded.\n");
-    return TEST_FAIL;
-  }
+
+  ASSERT(ck_create_kings_page_1(&args, &page) == CK_ERR_INVALID_CAN_ID,
+         "setting illegal standard base number should fail.");
 
   args.base_no = illegal_can_ext_id;
   args.has_extended_id = true;
   memset(&page, 0, sizeof(page));
-  if (ck_create_kings_page_1(&args, &page) != CK_ERR_INVALID_CAN_ID) {
-    printf("KP1: setting illegal extended base number succeeded.\n");
-    return TEST_FAIL;
-  }
 
-  return TEST_PASS;
+  ASSERT(ck_create_kings_page_1(&args, &page) == CK_ERR_INVALID_CAN_ID,
+         "setting illegal extended base number should fail.");
 }
 
-static test_err_t test_kp2(void) {
+void test_kp2(void) {
   ck_kp2_args_t args = {
       .address = test_city_address,
       .envelope =
@@ -140,94 +119,76 @@ static test_err_t test_kp2(void) {
   };
 
   ck_page_t page;
-  if (ck_create_kings_page_2(&args, &page) != CK_OK) {
-    printf("KP2: failed to create king's page.\n");
-    return TEST_FAIL;
-  }
-  if (verify_kp_header(2, &page) != TEST_PASS) {
-    return TEST_FAIL;
-  }
+  ASSERT(ck_create_kings_page_2(&args, &page) == CK_OK, "");
+
+  verify_kp_header(2, &page);
 
   uint32_t got_envelope_no = 0;
   memcpy(&got_envelope_no, &page.lines[2], sizeof(got_envelope_no));
   got_envelope_no &= CK_CAN_ID_MASK;
-  if (got_envelope_no != args.envelope.envelope_no) {
-    printf("KP2: wrong envelope number, expected: %u, got: %u.\n",
-           args.envelope.envelope_no, got_envelope_no);
-    return TEST_FAIL;
-  }
+
+  ASSERT(got_envelope_no == args.envelope.envelope_no,
+         "wrong envelope number, expected: %u, got: %u.",
+         args.envelope.envelope_no, got_envelope_no);
 
   bool got_has_extended_id = (page.lines[5] >> 7) & 0x01;
-  if (got_has_extended_id != args.envelope.has_extended_id) {
-    printf("KP2: wrong extended ID flag, expected: %u, got: %u.\n",
-           args.envelope.has_extended_id, got_has_extended_id);
-    return TEST_FAIL;
-  }
 
-  if (page.lines[6] != args.folder_no) {
-    printf("KP2: wrong folder number, expected: %u, got: %u.\n", args.folder_no,
-           page.lines[6]);
-    return TEST_FAIL;
-  }
+  ASSERT(got_has_extended_id == args.envelope.has_extended_id,
+         "wrong extended ID flag, expected: %u, got: %u.",
+         args.envelope.has_extended_id, got_has_extended_id);
+
+  ASSERT(page.lines[6] == args.folder_no,
+         "wrong folder number, expected: %u, got: %u.", args.folder_no,
+         page.lines[6]);
 
   bool got_enable_envelope = page.lines[7] & 0x01;
-  if (got_enable_envelope != args.envelope.enable) {
-    printf("KP2: wrong enable envelope flag, expected: %u, got: %u.\n",
-           args.envelope.enable, got_enable_envelope);
-    return TEST_FAIL;
-  }
+
+  ASSERT(got_enable_envelope == args.envelope.enable,
+         "wrong enable envelope flag, expected: %u, got: %u.",
+         args.envelope.enable, got_enable_envelope);
 
   ck_envelope_action_t got_envelope_action =
       (page.lines[7] >> 1) & 0x03;  // Two bits
-  if (got_envelope_action != args.envelope_action) {
-    printf("KP2: wrong envelope action, expected: %u, got: %u.\n",
-           args.envelope_action, got_envelope_action);
-    return TEST_FAIL;
-  }
+
+  ASSERT(got_envelope_action == args.envelope_action,
+         "wrong envelope action, expected: %u, got: %u.", args.envelope_action,
+         got_envelope_action);
 
   // Test illegal envelope numbers
   args.envelope.envelope_no = illegal_can_std_id;
   args.envelope.has_extended_id = false;
   memset(&page, 0, sizeof(page));
-  if (ck_create_kings_page_2(&args, &page) != CK_ERR_INVALID_CAN_ID) {
-    printf("KP2: setting illegal standard envelope number succeeded.\n");
-    return TEST_FAIL;
-  }
+
+  ASSERT(ck_create_kings_page_2(&args, &page) == CK_ERR_INVALID_CAN_ID,
+         "setting illegal standard envelope number should fail.");
 
   args.envelope.envelope_no = illegal_can_ext_id;
   args.envelope.has_extended_id = true;
   memset(&page, 0, sizeof(page));
-  if (ck_create_kings_page_2(&args, &page) != CK_ERR_INVALID_CAN_ID) {
-    printf("KP2: setting illegal extended envelope number succeeded.\n");
-    return TEST_FAIL;
-  }
+
+  ASSERT(ck_create_kings_page_2(&args, &page) == CK_ERR_INVALID_CAN_ID,
+         "setting illegal extended envelope number should fail.");
 
   // Test illegal parameter combination (enable = true && expel)
   args.envelope.envelope_no = 456;
   args.envelope.enable = true;
   args.envelope_action = CK_ENVELOPE_EXPEL;
   memset(&page, 0, sizeof(page));
-  if (ck_create_kings_page_2(&args, &page) != CK_ERR_INCOMPATIBLE_PARAMS) {
-    printf("KP2: setting illegal parameter combination succeeded.\n");
-    return TEST_FAIL;
-  }
 
-  return TEST_PASS;
+  ASSERT(ck_create_kings_page_2(&args, &page) == CK_ERR_INCOMPATIBLE_PARAMS,
+         "setting illegal parameter combination should fail.");
 }
 
-static test_err_t test_kp8(void) {
+void test_kp8(void) {
   ck_page_t page;
   ck_can_bit_timing_t default_bit_timing = ck_default_bit_timing();
-  if (ck_create_kings_page_8(test_city_address, &default_bit_timing, &page) !=
-      CK_OK) {
-    printf("KP8: failed to set default bit timing.\n");
-    return TEST_FAIL;
-  }
 
-  return TEST_PASS;
+  ASSERT(ck_create_kings_page_8(test_city_address, &default_bit_timing,
+                                &page) == CK_OK,
+         "failed to set default bit timing.");
 }
 
-static test_err_t test_kp16(void) {
+void test_kp16(void) {
   ck_kp16_args_t args = {
       .address = test_city_address,
       .folder_no = 78,
@@ -241,83 +202,65 @@ static test_err_t test_kp16(void) {
   };
 
   ck_page_t page;
-  if (ck_create_kings_page_16(&args, &page) != CK_OK) {
-    printf("KP16: failed to create king's page.\n");
-    return TEST_FAIL;
-  }
-  if (verify_kp_header(16, &page) != TEST_PASS) {
-    return TEST_FAIL;
-  }
+
+  ASSERT(ck_create_kings_page_16(&args, &page) == CK_OK, "");
+
+  verify_kp_header(16, &page);
 
   uint8_t got_dlc = page.lines[3] & 0x0F;
-  if (got_dlc != args.dlc) {
-    printf("KP16: wrong DLC, expected: %u, got: %u.\n", args.dlc, got_dlc);
-    return TEST_FAIL;
-  }
+
+  ASSERT(got_dlc == args.dlc, "wrong DLC, expected: %u, got: %u.", args.dlc,
+         got_dlc);
 
   bool got_has_rtr = (page.lines[3] >> 6) & 0x01;
-  if (got_has_rtr != args.has_rtr) {
-    printf("KP16: wrong has_rtr, expected: %u, got: %u.\n", args.has_rtr,
-           got_has_rtr);
-    return TEST_FAIL;
-  }
+
+  ASSERT(got_has_rtr == args.has_rtr, "wrong has_rtr, expected: %u, got: %u.",
+         args.has_rtr, got_has_rtr);
 
   ck_direction_t got_direction = page.lines[4] & 0x01;
-  if (got_direction != args.direction) {
-    printf("KP16: wrong direction, expected: %u, got: %u.\n", args.direction,
-           got_direction);
-    return TEST_FAIL;
-  }
+
+  ASSERT(got_direction == args.direction,
+         "wrong direction, expected: %u, got: %u.", args.direction,
+         got_direction);
 
   ck_document_action_t got_document_action =
       (page.lines[4] >> 4) & 0x03;  // Two bits
-  if (got_document_action != args.document_action) {
-    printf("KP16: wrong document action, expected: %u, got: %u.\n",
-           args.document_action, got_document_action);
-    printf("0x%x", page.lines[4]);
-    return TEST_FAIL;
-  }
+
+  ASSERT(got_document_action == args.document_action,
+         "wrong document action, expected: %u, got: %u.", args.document_action,
+         got_document_action);
 
   bool got_enable_folder = (page.lines[4] >> 6) & 0x01;
-  if (got_enable_folder != args.enable_folder) {
-    printf("KP16: wrong enable folder flag, expected: %u, got: %u.\n",
-           args.enable_folder, got_enable_folder);
-    return TEST_FAIL;
-  }
 
-  if (page.lines[5] != args.list_no) {
-    printf("KP16: wrong list number, expected: %u, got: %u.\n", args.list_no,
-           page.lines[5]);
-    return TEST_FAIL;
-  }
+  ASSERT(got_enable_folder == args.enable_folder,
+         "KP16: wrong enable folder flag, expected: %u, got: %u.",
+         args.enable_folder, got_enable_folder);
 
-  if (page.lines[6] != args.document_no) {
-    printf("KP16: wrong document number, expected: %u, got: %u.\n",
-           args.document_no, page.lines[6]);
-    return TEST_FAIL;
-  }
+  ASSERT(page.lines[5] == args.list_no,
+         "wrong list number, expected: %u, got: %u.", args.list_no,
+         page.lines[5]);
+
+  ASSERT(page.lines[6] == args.document_no,
+         "wrong document number, expected: %u, got: %u.", args.document_no,
+         page.lines[6]);
 
   // Test illegal folder number
   args.folder_no = 1;
   memset(&page, 0, sizeof(page));
-  if (ck_create_kings_page_16(&args, &page) != CK_ERR_INVALID_FOLDER_NUMBER) {
-    printf("KP16: setting illegal folder number succeeded.\n");
-    return TEST_FAIL;
-  }
+
+  ASSERT(ck_create_kings_page_16(&args, &page) == CK_ERR_INVALID_FOLDER_NUMBER,
+         "setting illegal folder number should fail.");
 
   // Test illegal DLC
   args.folder_no = 78;
   args.dlc = CK_CAN_MAX_DLC + 1;
   memset(&page, 0, sizeof(page));
-  if (ck_create_kings_page_16(&args, &page) != CK_ERR_INVALID_CAN_DLC) {
-    printf("KP16: setting illegal DLC succeeded.\n");
-    return TEST_FAIL;
-  }
 
-  return TEST_PASS;
+  ASSERT(ck_create_kings_page_16(&args, &page) == CK_ERR_INVALID_CAN_DLC,
+         "setting illegal DLC should fail.");
 }
 
-static test_err_t test_kp17(void) {
+void test_kp17(void) {
   ck_kp17_args_t args = {
       .address = test_city_address,
       .direction = CK_DIRECTION_TRANSMIT,
@@ -330,79 +273,56 @@ static test_err_t test_kp17(void) {
   };
 
   ck_page_t page;
-  if (ck_create_kings_page_17(&args, &page) != CK_OK) {
-    printf("KP17: failed to create king's page.\n");
-    return TEST_FAIL;
-  }
-  if (verify_kp_header(17, &page) != TEST_PASS) {
-    return TEST_FAIL;
-  }
+
+  ASSERT(ck_create_kings_page_17(&args, &page) == CK_OK, "");
+
+  verify_kp_header(17, &page);
 
   ck_direction_t got_direction = page.lines[2] & 0x01;
-  if (got_direction != args.direction) {
-    printf("KP17: wrong direction, expected: %u, got: %u.\n", args.direction,
-           got_direction);
-    return TEST_FAIL;
-  }
+
+  ASSERT(got_direction == args.direction,
+         "wrong direction, expected: %u, got: %u.", args.direction,
+         got_direction);
 
   ck_list_type_t got_list_type = (page.lines[2] >> 3) & 0x03;  // Two bits
-  if (got_list_type != args.list_type) {
-    printf("KP17: wrong list type, expected: %x, got: %x.\n", args.list_type,
-           got_list_type);
-    return TEST_FAIL;
-  }
 
-  if (page.lines[3] != args.source_list_no) {
-    printf("KP17: wrong source list number, expected: %u, got: %u.\n",
-           args.source_list_no, page.lines[3]);
-    return TEST_FAIL;
-  }
+  ASSERT(got_list_type == args.list_type,
+         "wrong list type, expected: %x, got: %x.", args.list_type,
+         got_list_type);
 
-  if (page.lines[4] != args.source_record_no) {
-    printf("KP17: wrong source record number, expected: %u, got: %u.\n",
-           args.source_record_no, page.lines[4]);
-    return TEST_FAIL;
-  }
+  ASSERT(page.lines[3] == args.source_list_no,
+         "wrong source list number, expected: %u, got: %u.",
+         args.source_list_no, page.lines[3]);
 
-  if (page.lines[5] != args.target_list_no) {
-    printf("KP17: wrong target list number, expected: %u, got: %u.\n",
-           args.target_list_no, page.lines[5]);
-    return TEST_FAIL;
-  }
+  ASSERT(page.lines[4] == args.source_record_no,
+         "wrong source record number, expected: %u, got: %u.",
+         args.source_record_no, page.lines[4]);
 
-  if (page.lines[6] != args.target_record_no) {
-    printf("KP17: wrong target record number, expected: %u, got: %u.\n",
-           args.target_record_no, page.lines[6]);
-    return TEST_FAIL;
-  }
+  ASSERT(page.lines[5] == args.target_list_no,
+         "wrong target list number, expected: %u, got: %u.",
+         args.target_list_no, page.lines[5]);
 
-  if (page.lines[7] != args.position) {
-    printf("KP17: wrong position, expected: %u, got: %u.\n", args.position,
-           page.lines[7]);
-    return TEST_FAIL;
-  }
+  ASSERT(page.lines[6] == args.target_record_no,
+         "wrong target record number, expected: %u, got: %u.",
+         args.target_record_no, page.lines[6]);
 
-  return TEST_PASS;
+  ASSERT(page.lines[7] == args.position,
+         "wrong position, expected: %u, got: %u.", args.position,
+         page.lines[7]);
 }
 
 // These verifications are the same for all king's pages.
-static test_err_t verify_kp_header(int page_no, ck_page_t *page) {
-  if (page->line_count != CK_MAX_LINES_PER_PAGE) {
-    printf("KP%d: wrong line count, expected: %u, got: %u.\n", page_no,
-           CK_MAX_LINES_PER_PAGE, page->line_count);
-    return TEST_FAIL;
-  }
-  if (page->lines[0] != test_city_address) {
-    printf("KP%d: wrong address, expected: %u, got: %u.\n", page_no,
-           test_city_address, page->lines[0]);
-    return TEST_FAIL;
-  }
-  if (page->lines[1] != page_no) {
-    printf("KP%d: wrong page number, expected: %d, got: %u.\n", page_no,
-           page_no, page->lines[1]);
-    return TEST_FAIL;
-  }
-  return TEST_PASS;
-}
+void verify_kp_header(int page_no, ck_page_t *page) {
+  ASSERT(page->line_count == CK_MAX_LINES_PER_PAGE,
+         "KP%d: wrong line count, expected: %u, got: %u.", page_no,
+         CK_MAX_LINES_PER_PAGE, page->line_count);
 
+  ASSERT(page->lines[0] == test_city_address,
+         "KP%d: wrong address, expected: %u, got: %u.", page_no,
+         test_city_address, page->lines[0]);
+
+  ASSERT(page->lines[1] == page_no,
+         "KP%d: wrong page number, expected: %d, got: %u.", page_no, page_no,
+         page->lines[1]);
+}
 // NOLINTEND(*-magic-numbers)
--- libs/can-kingdom/tests/types-test.c
+++ libs/can-kingdom/tests/types-test.c
@@ -1,162 +1,83 @@
 #include "types.h"
 
-#include <assert.h>
-#include <stdio.h>
 #include <string.h>
 
 #include "test.h"
 
-static test_err_t test_check_action_mode(void);
-static test_err_t test_check_comm_mode(void);
-static test_err_t test_check_list_type(void);
-static test_err_t test_check_can_bit_timing(void);
-static test_err_t test_default_letter(void);
+void test_check_action_mode(void);
+void test_check_comm_mode(void);
+void test_check_list_type(void);
+void test_check_can_bit_timing(void);
+void test_default_letter(void);
 
 int main(void) {
-  assert(test_check_action_mode() == TEST_PASS);
-  assert(test_check_comm_mode() == TEST_PASS);
-  assert(test_check_list_type() == TEST_PASS);
-  assert(test_check_can_bit_timing() == TEST_PASS);
-  assert(test_default_letter() == TEST_PASS);
+  test_check_action_mode();
+  test_check_comm_mode();
+  test_check_list_type();
+  test_check_can_bit_timing();
+  test_default_letter();
 }
 
-static test_err_t test_check_action_mode(void) {
-  ck_action_mode_t mode = CK_ACTION_MODE_KEEP_CURRENT;
-  if (ck_check_action_mode(mode) != CK_OK) {
-    printf("check_action_mode: valid mode returns error.\n");
-    return TEST_FAIL;
-  }
-  mode = CK_ACTION_MODE_RUN;
-  if (ck_check_action_mode(mode) != CK_OK) {
-    printf("check_action_mode: valid mode returns error.\n");
-    return TEST_FAIL;
-  }
-  mode = CK_ACTION_MODE_FREEZE;
-  if (ck_check_action_mode(mode) != CK_OK) {
-    printf("check_action_mode: valid mode returns error.\n");
-    return TEST_FAIL;
-  }
-  mode = CK_ACTION_MODE_RESET;
-  if (ck_check_action_mode(mode) != CK_OK) {
-    printf("check_action_mode: valid mode returns error.\n");
-    return TEST_FAIL;
-  }
+void test_check_action_mode(void) {
+  ASSERT(ck_check_action_mode(CK_ACTION_MODE_KEEP_CURRENT) == CK_OK, "");
+  ASSERT(ck_check_action_mode(CK_ACTION_MODE_RUN) == CK_OK, "");
+  ASSERT(ck_check_action_mode(CK_ACTION_MODE_FREEZE) == CK_OK, "");
+  ASSERT(ck_check_action_mode(CK_ACTION_MODE_RESET) == CK_OK, "");
+
   const int invalid_mode = 8;
-  mode = invalid_mode;
-  if (ck_check_action_mode(mode) == CK_OK) {
-    printf("check_action_mode: invalid mode returns OK.\n");
-    return TEST_FAIL;
-  }
-  return TEST_PASS;
+  ASSERT(ck_check_action_mode(invalid_mode) != CK_OK, "");
 }
 
-static test_err_t test_check_comm_mode(void) {
-  ck_comm_mode_t mode = CK_COMM_MODE_KEEP_CURRENT;
-  if (ck_check_comm_mode(mode) != CK_OK) {
-    printf("check_comm_mode: valid mode returns error.\n");
-    return TEST_FAIL;
-  }
-  mode = CK_COMM_MODE_SILENT;
-  if (ck_check_comm_mode(mode) != CK_OK) {
-    printf("check_comm_mode: valid mode returns error.\n");
-    return TEST_FAIL;
-  }
-  mode = CK_COMM_MODE_LISTEN_ONLY;
-  if (ck_check_comm_mode(mode) != CK_OK) {
-    printf("check_comm_mode: valid mode returns error.\n");
-    return TEST_FAIL;
-  }
-  mode = CK_COMM_MODE_COMMUNICATE;
-  if (ck_check_comm_mode(mode) != CK_OK) {
-    printf("check_comm_mode: valid mode returns error.\n");
-    return TEST_FAIL;
-  }
+void test_check_comm_mode(void) {
+  ASSERT(ck_check_comm_mode(CK_COMM_MODE_KEEP_CURRENT) == CK_OK, "");
+  ASSERT(ck_check_comm_mode(CK_COMM_MODE_SILENT) == CK_OK, "");
+  ASSERT(ck_check_comm_mode(CK_COMM_MODE_LISTEN_ONLY) == CK_OK, "");
+  ASSERT(ck_check_comm_mode(CK_COMM_MODE_COMMUNICATE) == CK_OK, "");
+
   const int invalid_mode = 8;
-  mode = invalid_mode;
-  if (ck_check_comm_mode(mode) == CK_OK) {
-    printf("check_comm_mode: invalid mode returns OK.\n");
-    return TEST_FAIL;
-  }
-  return TEST_PASS;
+  ASSERT(ck_check_comm_mode(invalid_mode) != CK_OK, "");
 }
 
-static test_err_t test_check_list_type(void) {
-  ck_list_type_t type = CK_LIST_BIT;
-  if (ck_check_list_type(type) != CK_OK) {
-    printf("check_list_type: valid type returns error.\n");
-    return TEST_FAIL;
-  }
-  type = CK_LIST_LINE;
-  if (ck_check_list_type(type) != CK_OK) {
-    printf("check_list_type: valid type returns error.\n");
-    return TEST_FAIL;
-  }
-  type = CK_LIST_PAGE;
-  if (ck_check_list_type(type) != CK_OK) {
-    printf("check_list_type: valid type returns error.\n");
-    return TEST_FAIL;
-  }
-  type = CK_LIST_DOCUMENT;
-  if (ck_check_list_type(type) != CK_OK) {
-    printf("check_list_type: valid type returns error.\n");
-    return TEST_FAIL;
-  }
+void test_check_list_type(void) {
+  ASSERT(ck_check_list_type(CK_LIST_BIT) == CK_OK, "");
+  ASSERT(ck_check_list_type(CK_LIST_LINE) == CK_OK, "");
+  ASSERT(ck_check_list_type(CK_LIST_PAGE) == CK_OK, "");
+  ASSERT(ck_check_list_type(CK_LIST_DOCUMENT) == CK_OK, "");
+
   const int invalid_type = 8;
-  type = invalid_type;
-  if (ck_check_list_type(type) == CK_OK) {
-    printf("check_list_type: invalid type returns OK.\n");
-    return TEST_FAIL;
-  }
-  return TEST_PASS;
+  ASSERT(ck_check_list_type(invalid_type) != CK_OK, "");
 }
 
-static test_err_t test_check_can_bit_timing(void) {
+void test_check_can_bit_timing(void) {
   ck_can_bit_timing_t bit_timing = ck_default_bit_timing();
-  if (ck_check_can_bit_timing(&bit_timing) != CK_OK) {
-    printf("check_can_bit_timing: valid bit timing returns error.\n");
-    return TEST_FAIL;
-  }
+  ASSERT(ck_check_can_bit_timing(&bit_timing) == CK_OK, "");
 
-  // Illegal parameters test
   ck_can_bit_timing_t illegal_prescaler = bit_timing;
-  ck_can_bit_timing_t illegal_tq = bit_timing;
-  ck_can_bit_timing_t illegal_ps2 = bit_timing;
-  ck_can_bit_timing_t illegal_sjw = bit_timing;
-
   illegal_prescaler.prescaler = 0;
+  ASSERT(ck_check_can_bit_timing(&illegal_prescaler) != CK_OK, "");
+
+  ck_can_bit_timing_t illegal_tq = bit_timing;
   illegal_tq.time_quanta = 0;
+  ASSERT(ck_check_can_bit_timing(&illegal_tq) != CK_OK, "");
+
+  ck_can_bit_timing_t illegal_ps2 = bit_timing;
   illegal_ps2.phase_seg2 = 0;
+  ASSERT(ck_check_can_bit_timing(&illegal_ps2) != CK_OK, "");
+
+  ck_can_bit_timing_t illegal_sjw = bit_timing;
   illegal_sjw.sjw = 0;
-
-  if (ck_check_can_bit_timing(&illegal_prescaler) == CK_OK) {
-    printf("check_can_bit_timing: illegal prescaler returned OK.\n");
-    return TEST_FAIL;
-  }
-  if (ck_check_can_bit_timing(&illegal_tq) == CK_OK) {
-    printf("check_can_bit_timing: illegal time quanta returned OK.\n");
-    return TEST_FAIL;
-  }
-  if (ck_check_can_bit_timing(&illegal_ps2) == CK_OK) {
-    printf("check_can_bit_timing: illegal phase seg 2 returned OK.\n");
-    return TEST_FAIL;
-  }
-  if (ck_check_can_bit_timing(&illegal_sjw) == CK_OK) {
-    printf("check_can_bit_timing: illegal sjw returned OK.\n");
-    return TEST_FAIL;
-  }
-
-  return TEST_PASS;
+  ASSERT(ck_check_can_bit_timing(&illegal_sjw) != CK_OK, "");
 }
 
-static test_err_t test_default_letter(void) {
+void test_default_letter(void) {
   ck_letter_t letter = ck_default_letter();
-  if (letter.envelope.envelope_no != CK_DEFAULT_LETTER_ENVELOPE) {
-    printf("default_letter: incorrect envelope number, expected: %u, got: %u\n",
-           CK_DEFAULT_LETTER_ENVELOPE, letter.envelope.envelope_no);
-    return TEST_FAIL;
-  }
+  ASSERT(letter.envelope.envelope_no == CK_DEFAULT_LETTER_ENVELOPE,
+         "incorrect envelope number, expected: %u, got: %u",
+         CK_DEFAULT_LETTER_ENVELOPE, letter.envelope.envelope_no);
+
   const uint8_t expected_data[CK_MAX_LINES_PER_PAGE] = {0xAA, 0xAA, 0xAA, 0xAA,
                                                         0xAA, 0xAA, 0xAA, 0xAA};
+
   if (memcmp(letter.page.lines, expected_data, sizeof(expected_data)) != 0) {
     printf("default_letter: incorrect page data, expected:");
     for (uint8_t i = 0; i < CK_MAX_LINES_PER_PAGE; i++) {
@@ -166,8 +87,8 @@ static test_err_t test_default_letter(void) {
     for (uint8_t i = 0; i < letter.page.line_count; i++) {
       printf(" %u,", letter.page.lines[i]);
     }
-    printf(".\n");
-    return TEST_FAIL;
+    printf(".");
+
+    ASSERT(false, "");
   }
-  return TEST_PASS;
 }
