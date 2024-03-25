63e8c90f72add59233c7d68148284b26cb76fdbd
Hashem Hashem <hashem@canedudev.com>
2024-03-25 16:48:40 +0100

    chore(release): check for release zip before creating release

--- scripts/create-release.sh
+++ scripts/create-release.sh
@@ -5,6 +5,18 @@ set -eo pipefail
 TAG=$(git describe --tags --always)
 RELEASE_FILE="rover-release-${TAG}.zip"
 
+echo
+echo "Creating release..."
+
+if [[ -f ${RELEASE_FILE} ]]; then
+	echo
+	echo "ERROR: File ${PWD}/${RELEASE_FILE} already exists."
+	echo "Remove it or create a new tag before retrying."
+	echo "Exiting..."
+	echo
+	exit 1
+fi
+
 # Add all .bin files
 find . -type f -name "*.bin" | zip -q -j "${RELEASE_FILE}" -@
 
