992671101324cad6c87ab8bd36a46de1d059e236
Hashem Hashem <hashem@canedudev.com>
2024-09-27 09:54:04 +0200

    chore: rename wheel to brake

--- rover_py/tests/test_brake_conf.py
+++ rover_py/tests/test_brake_conf.py
@@ -2,7 +2,7 @@ import time
 
 from canlib import canlib
 
-from ..rover import rover, wheel
+from ..rover import brake, rover
 
 with canlib.openChannel(
     channel=0,
@@ -16,7 +16,7 @@ with canlib.openChannel(
 
     print("Testing report period...")
     # Set report period to 1s
-    ch.writeWait(wheel.set_report_period_frame(1000), -1)
+    ch.writeWait(brake.set_report_period_frame(1000), -1)
 
     # Measure time between two reports
     t_before = time.time()
@@ -32,14 +32,14 @@ with canlib.openChannel(
     assert t_after - t_before > 1
 
     # Restore report period
-    ch.writeWait(wheel.set_report_period_frame(200), -1)
+    ch.writeWait(brake.set_report_period_frame(200), -1)
 
     print(
         "Testing setting incorrect wheel parameters. Data should be abnormally high. Check using logger."
     )
-    ch.writeWait(wheel.set_wheel_parameters_frame(5, 1), -1)
+    ch.writeWait(brake.set_wheel_parameters_frame(5, 1), -1)
 
     input("Press Enter to continue...")
 
     # Restore defaults
-    ch.writeWait(wheel.set_wheel_parameters_frame(45, 0.16), -1)
+    ch.writeWait(brake.set_wheel_parameters_frame(45, 0.16), -1)
