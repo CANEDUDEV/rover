8c5bd4359a86843e11a30aca950c773c89912240
Hashem Hashem <hashem@canedudev.com>
2024-10-22 12:18:42 +0200

    feat(candump): add log to file functionality

--- rover_py/candump.py
+++ rover_py/candump.py
@@ -1,66 +1,186 @@
 import argparse
 import sys
+import time
+from pathlib import Path
 
 import can
 import cantools
 
-parser = argparse.ArgumentParser(description="dump rover CAN messages in DBC format")
-parser.add_argument(
-    "-l", "--list", action="store_true", help="list connected CAN interfaces and exit"
-)
-parser.add_argument(
-    "-i", "--interface", help="type of interface to use, defaults to first found"
-)
-parser.add_argument("-c", "--channel", help="channel to use, defaults to first found")
-parser.add_argument(
-    "-b",
-    "--bitrate",
-    type=int,
-    default=125000,
-    help="CAN bus bitrate (default: 125000)",
-)
-parser.add_argument("--dbc", help="CAN DBC file for decoding messages")
 
-args = parser.parse_args()
+def main():
+    parser = argparse.ArgumentParser(
+        description="dump rover CAN messages in DBC format"
+    )
+    parser.add_argument(
+        "-l",
+        "--list",
+        action="store_true",
+        help="list connected CAN interfaces and exit",
+    )
+    parser.add_argument(
+        "-i", "--interface", help="type of interface to use, defaults to first found"
+    )
+    parser.add_argument(
+        "-c", "--channel", help="channel to use, defaults to first found"
+    )
+    parser.add_argument(
+        "-b",
+        "--bitrate",
+        type=int,
+        default=125000,
+        help="CAN bus bitrate (default: 125000)",
+    )
+    parser.add_argument(
+        "--dbc", metavar="DBC_FILE", help="CAN DBC file for decoding messages"
+    )
+    parser.add_argument("--log", metavar="FILE", help="log output to file")
 
-configs = can.detect_available_configs(interfaces=["kvaser", "socketcan"])
-if len(configs) < 1:
-    print("No CAN interfaces connected")
-    sys.exit(1)
+    args = parser.parse_args()
+    Dumper().from_args(args).start()
 
 
-if args.list:
-    print("Found interfaces:")
-    for interface in configs:
-        print(f"\t{interface}")
-    sys.exit(0)
+class Dumper:
+    def __init__(
+        self, interface=None, channel=None, db=None, logger=None, bitrate=None
+    ):
+        self.interface = interface
+        self.channel = channel
+        self.db = db
+        self.logger = logger
+        self.bitrate = bitrate
 
-db = None
-if args.dbc:
-    try:
-        db = cantools.db.load_file(args.dbc)
-    except:
-        print(f"error: {args.dbc} is not a valid DBC file", file=sys.stderr)
+    def start(self):
+        try:
+            bus = can.ThreadSafeBus(
+                interface=self.interface, channel=self.channel, bitrate=self.bitrate
+            )
+        except can.exceptions.CanError as e:
+            print(f"error: failed to connect to CAN bus: {e}", file=sys.stderr)
+            print(f"\nconfig:", file=sys.stderr)
+            print(f"\tinterface: {self.interface}", file=sys.stderr)
+            print(f"\tchannel: {self.channel}", file=sys.stderr)
+            print(f"\tbitrate: {self.bitrate}", file=sys.stderr)
+            sys.exit(1)
+
+        notifier = can.Notifier(bus, [CanPrinter(self.db)])
+        if self.logger:
+            notifier.add_listener(self.logger)
+
+        try:
+            while True:
+                time.sleep(1)
+
+        except KeyboardInterrupt:
+            notifier.stop(timeout=30)  # in seconds
+            bus.shutdown()
+
+    def from_args(self, args):
+        configs = can.detect_available_configs(interfaces=["kvaser", "socketcan"])
+
+        if args.list:
+            print("Found interfaces:")
+            for interface in configs:
+                print(f"\t{interface}")
+            sys.exit(0)
+
+        if len(configs) < 1:
+            print("No CAN interfaces connected")
+            sys.exit(1)
+
+        self.__parse_interface(configs, args.interface, args.channel)
+        self.__parse_db(args.dbc)
+        self.__parse_logfile(args.log)
+        self.bitrate = args.bitrate
+
+        return self
+
+    def __parse_interface(self, configs, interface, channel):
+        if not (interface or channel):
+            self.interface = configs[0]["interface"]
+            self.channel = configs[0]["channel"]
+            return
+
+        found_interfaces = [c["interface"] for c in configs]
+        found_channels = [c["channel"] for c in configs]
+
+        if interface and interface not in found_interfaces:
+            print(
+                f"error: interface {interface} not found in {found_interfaces}",
+                file=sys.stderr,
+            )
+            sys.exit(1)
+
+        if channel and channel not in found_channels:
+            print(
+                f"error: channel {channel} not found in {found_channels}",
+                file=sys.stderr,
+            )
+            sys.exit(1)
+
+        for c in configs:
+            # Check that the found interface and channel belong to the same config
+            if c["interface"] == interface and c["channel"] == channel:
+                self.interface = interface
+                self.channel = channel
+                return
+
+        print(
+            f"error: the combination ({interface}, {channel}) was not found in configs. configs: {configs}",
+            file=sys.stderr,
+        )
         sys.exit(1)
 
-if args.interface or args.channel:
-    interface = args.interface
-    channel = args.channel
-else:
-    interface = configs[0]["interface"]
-    channel = configs[0]["channel"]
+    def __parse_db(self, dbc):
+        if not dbc:
+            return
 
-with can.ThreadSafeBus(
-    interface=interface, channel=channel, bitrate=args.bitrate
-) as bus:
-    try:
-        for msg in bus:
-            if db:
-                print(
-                    db.decode_message(msg.arbitration_id, msg.data)  # pyright: ignore
-                )
-            else:
-                print(msg)
+        try:
+            self.db = cantools.db.load_file(dbc)
+        except:
+            print(f"error: {dbc} is not a valid DBC file", file=sys.stderr)
+            sys.exit(1)
 
-    except KeyboardInterrupt:
-        pass
+    def __parse_logfile(self, log_file):
+        if not log_file:
+            return
+
+        suffix = Path(log_file).suffix
+
+        supported_suffixes = [
+            ".asc",
+            ".blf",
+            ".csv",
+            ".db ",
+            ".log",
+            ".mf4",
+            ".trc",
+            ".txt",
+        ]
+
+        if suffix not in supported_suffixes:
+            print(
+                f"error: unsupported log file format: {suffix}. Must be one of {supported_suffixes}",
+                file=sys.stderr,
+            )
+            sys.exit(1)
+
+        try:
+            self.logger = can.Logger(log_file)
+        except OSError as e:
+            print(f"error: couldn't open log file: {e}", file=sys.stderr)
+            sys.exit(1)
+
+
+class CanPrinter(can.Listener):
+    def __init__(self, db):
+        self.db = db
+
+    def on_message_received(self, msg):
+        if self.db:
+            print(self.db.decode_message(msg.arbitration_id, msg.data))
+        else:
+            print(msg)
+
+
+if __name__ == "__main__":
+    main()
