13818f61aca84d6675673ddffff09739a727af1f
Hashem Hashem <hashem@canedudev.com>
2024-10-22 10:35:25 +0200

    feat: use python-can and cantools in candump
    This enables use of both socketcan and Kvaser CANlib.

--- rover_py/candump.py
+++ rover_py/candump.py
@@ -1,19 +1,66 @@
+import argparse
 import sys
-from binascii import hexlify
 
-from canlib import canlib
+import can
+import cantools
 
-flags = canlib.Open.NO_INIT_ACCESS
-if len(sys.argv) > 1 and sys.argv[1] == "set-bitrate":
-    flags = canlib.Open.REQUIRE_INIT_ACCESS
+parser = argparse.ArgumentParser(description="dump rover CAN messages in DBC format")
+parser.add_argument(
+    "-l", "--list", action="store_true", help="list connected CAN interfaces and exit"
+)
+parser.add_argument(
+    "-i", "--interface", help="type of interface to use, defaults to first found"
+)
+parser.add_argument("-c", "--channel", help="channel to use, defaults to first found")
+parser.add_argument(
+    "-b",
+    "--bitrate",
+    type=int,
+    default=125000,
+    help="CAN bus bitrate (default: 125000)",
+)
+parser.add_argument("--dbc", help="CAN DBC file for decoding messages")
 
-with canlib.openChannel(channel=0, flags=flags, bitrate=canlib.canBITRATE_125K) as ch:
-    ch.busOn()
-    while True:
-        try:
-            frame = ch.read(timeout=1000)
-            print(
-                f'{frame.timestamp} id: {frame.id}, DLC: {frame.dlc}, Data: {hexlify(frame.data, " ")}'
-            )
-        except canlib.CanNoMsg:
-            pass
+args = parser.parse_args()
+
+configs = can.detect_available_configs(interfaces=["kvaser", "socketcan"])
+if len(configs) < 1:
+    print("No CAN interfaces connected")
+    sys.exit(1)
+
+
+if args.list:
+    print("Found interfaces:")
+    for interface in configs:
+        print(f"\t{interface}")
+    sys.exit(0)
+
+db = None
+if args.dbc:
+    try:
+        db = cantools.db.load_file(args.dbc)
+    except:
+        print(f"error: {args.dbc} is not a valid DBC file", file=sys.stderr)
+        sys.exit(1)
+
+if args.interface or args.channel:
+    interface = args.interface
+    channel = args.channel
+else:
+    interface = configs[0]["interface"]
+    channel = configs[0]["channel"]
+
+with can.ThreadSafeBus(
+    interface=interface, channel=channel, bitrate=args.bitrate
+) as bus:
+    try:
+        for msg in bus:
+            if db:
+                print(
+                    db.decode_message(msg.arbitration_id, msg.data)  # pyright: ignore
+                )
+            else:
+                print(msg)
+
+    except KeyboardInterrupt:
+        pass
