219fed8674cd649e5e435635678ae38171bd3bbf
Hashem Hashem <hashem@canedudev.com>
2025-02-12 11:33:08 +0100

    chore(apps): remove unused joystick app
    It was meant as a test app but was left in the repo.

--- apps/joystick/src/interrupts.c
+++
@@ -1,7 +0,0 @@
-#include "peripherals.h"
-#include "stm32f3xx_hal.h"
-
-void DMA1_Channel1_IRQHandler(void) {
-  peripherals_t *peripherals = get_peripherals();
-  HAL_DMA_IRQHandler(&peripherals->hdma_adc1);
-}
